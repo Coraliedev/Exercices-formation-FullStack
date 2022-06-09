c9977c6b681ccf9358a2235b59ed3337fdb3b624
Coraliedev <coralie.acquaert@gmail.com>
2022-06-09 22:52:24 +0200

    modif qcm

--- quizjs/index.js
+++ quizjs/index.js
@@ -8,48 +8,51 @@ const title = document.createElement("h1");
 title.textContent = "QUESTIONNAIRE";
 body.appendChild(title);
 
-/* Creating an object with the questions and the answers. */
-let QCM = [
-  "Le premier club de foot à avoir gagné la coupe de france est:",
-  [
-    "Le FC Lyon",
-    "L'olympique de Marseille",
-    "Le sporting de Bondy",
+let QCM = {
+  questions: [
+    "Le premier club de foot à avoir gagné la coupe de france est:",
+    "Dans le film Casablanca,quelle musique l'orchestre interprète-t-il pour couvrir les chants des nazis?",
+    "Les guêpes de mer sont?",
+    "Laquelle de ces propositions, n’est pas une appellation du « Pierre Feuille Ciseaux » dans le monde ?",
+    "Parmi ces 3 voitures, laquelle a vraiment existé ?",
+  ],
+  responses: [
+    [
+      "Le FC Lyon",
+      "L'olympique de Marseille",
+      "Le sporting de Bondy",
+      "L'olympique de Pantin",
+    ],
+    [
+      "God save the queen",
+      "You'll never walk alone",
+      "La marseillaise",
+      "Lofteurs up and down",
+    ],
+    [
+      " Des poissons venimeux",
+      "Des méduses venimeuses",
+      "Le surnom des morpions du commandant cousteau",
+      "Des guêpes mais jetées à la mer, simplement",
+    ],
+    [
+      "Klet-Klat-Klout en Finlande",
+      "Schnick-Schnack-Schnuck en Allemagne ?",
+      "Kawi-Bawi-Bo en Corée",
+      "Chiam-Cham-Choum au Liban",
+    ],
+    ['La Skoda "Tapina"', 'La Nissan "gigolo"', 'La Mazda "la puta"'],
+  ],
+  responsesOk:[
     "L'olympique de Pantin",
-  ],
-  "Dans le film Casablanca,quelle musique l'orchestre interprète-t-il pour couvrir les chants des nazis?",
-  [
-    "God save the queen",
-    "You'll never walk alone",
     "La marseillaise",
-    "Lofteurs up and down",
-  ],
-  "Les guêpes de mer sont?",
-  [
-    " Des poissons venimeux",
     "Des méduses venimeuses",
-    "Le surnom des morpions du commandant cousteau",
-    "Des guêpes mais jetées à la mer, simplement",
-  ],
-  "Laquelle de ces propositions, n’est pas une appellation du « Pierre Feuille Ciseaux » dans le monde ?",
-  [
     "Klet-Klat-Klout en Finlande",
-    "Schnick-Schnack-Schnuck en Allemagne ?",
-    "Kawi-Bawi-Bo en Corée",
-    "Chiam-Cham-Choum au Liban",
+    'La Mazda "la puta"',
   ],
-  "Parmi ces 3 voitures, laquelle a vraiment existé ?",
-  ['La Skoda "Tapina"', 'La Nissan "gigolo"', 'La Mazda "la puta"'],
-];
+  responsePlayer:[]
+};
 
-/* An array of the correct answers. */
-let responseOk = [
-  "L'olympique de Pantin",
-  "La marseillaise",
-  "Des méduses venimeuses",
-  "Klet-Klat-Klout en Finlande",
-  'La Mazda "la puta"',
-];
 
 /* Creating a new element and assigning it to the variable divQCM. */
 let divQCM = document.createElement("div");
@@ -59,20 +62,31 @@ let divQCM = document.createElement("div");
  * question is added to the page.
  * @param e - the event object
  */
-function nextQuestion(e){
+function nextQuestion(e) {
   e.preventDefault();
-    divQCM.innerHTML="";
-    form.innerHTML=""
-    createQuestion(next);
+/* Getting the radio buttons that have the same name as the current question, and then it is looping
+through them to find the one that is checked. Then it is getting the id of the checked radio button
+and pushing it to the array. */
+  let radios = document.getElementsByName(next-1);
+  for(const element of radios){
+   if(element.checked){
+   let value = element.id;
+   QCM.responsePlayer.push(value);
+   }
+  }
+  console.log(QCM)
+  divQCM.innerHTML = "";
+  form.innerHTML = "";
+  createQuestion(next);
 }
 
-let button=document.createElement("input");
-button.setAttribute("type","submit");
-button.setAttribute("value","Valider")
+let button = document.createElement("input");
+button.setAttribute("type", "submit");
+button.setAttribute("value", "Valider");
 let form = document.createElement("form");
 divQCM.appendChild(form);
 
-let next=0;
+let next = 0;
 
 /**
  * It creates a paragraph element, adds the question to it, adds the paragraph to the div, adds the div
@@ -81,28 +95,27 @@ let next=0;
  * @param i - the index of the question in the array
  */
 function createQuestion(i) {
-  if (next<=8){  
-  let para = document.createElement("p");
-  para.textContent = `${QCM[i]}`;
-  divQCM.appendChild(para);
-  body.appendChild(divQCM);
-  for (const element of QCM[i+1]) {
-    let radio = document.createElement("input");
-    radio.setAttribute("type", "radio");
-    radio.setAttribute("name", i);
-    radio.setAttribute("id",element);
-    let label = document.createElement("label");
-    label.setAttribute("for", element);
-    label.textContent = element;
-    form.appendChild(radio);
-    form.appendChild(label);
-    divQCM.appendChild(form);
+  if (next <= 4) {
+    let para = document.createElement("p");
+    para.textContent = `${QCM.questions[i]}`;
+    divQCM.appendChild(para);
+    body.appendChild(divQCM);
+    for (const element of QCM.responses[i]) {
+      let radio = document.createElement("input");
+      radio.setAttribute("type", "radio");
+      radio.setAttribute("name", i);
+      radio.setAttribute("id", element);
+      let label = document.createElement("label");
+      label.setAttribute("for", element);
+      label.textContent = element;
+      form.appendChild(radio);
+      form.appendChild(label);
+      divQCM.appendChild(form);
+    }
+    form.appendChild(button);
+    next ++;
   }
-  form.appendChild(button);
-  next+=2;
-  }
-  else {console.log("café")}
 }
 
 createQuestion(0);
-form.addEventListener("submit",nextQuestion);
+form.addEventListener("submit", nextQuestion);
