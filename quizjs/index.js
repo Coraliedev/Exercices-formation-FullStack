8f9d40e809345b973d1f6ce23af1aee6e8ae687e
Coraliedev <coralie.acquaert@gmail.com>
2022-06-12 15:55:40 +0200

    modif quiz

--- quizjs/index.js
+++ quizjs/index.js
@@ -1,137 +1,178 @@
-// "use strict";
+"use strict";
 
-// /* Selecting the body element and assigning it to the variable body. */
-// const body = document.querySelector("body");
+let section = document.getElementById("section");
+let next = 0;
 
-// /* Creating a new element and assigning it to the variable title. */
-// const title = document.createElement("h1");
-// title.textContent = "QUESTIONNAIRE";
-// body.appendChild(title);
+/* Creating a new element and assigning it to the variable title. */
+const title = document.createElement("h1");
+title.textContent = "QUESTIONNAIRE";
+section.appendChild(title);
 
-// /* Creating an object with the questions, the answers, the correct answers, and an empty array for the
-// player's answers. */
-// let QCM = [
-//   {question: "question", reponse1: "reponse1", reponse2: "reponse2", reponse3: "reponse3",reponse4: "reponse4", attendu: "attendu", utilisateur: ""},
-//   {question: "question", reponse1: "reponse1", reponse2: "reponse2", reponse3: "reponse3",reponse4: "reponse4", attendu: "attendu", utilisateur: ""},
-//   {question: "question", reponse1: "reponse1", reponse2: "reponse2", reponse3: "reponse3",reponse4: "reponse4", attendu: "attendu", utilisateur: ""},
-//   {question: "question", reponse1: "reponse1", reponse2: "reponse2", reponse3: "reponse3",reponse4: "reponse4", attendu: "attendu", utilisateur: ""},
-//   {question: "question", reponse1: "reponse1", reponse2: "reponse2", reponse3: "reponse3",reponse4: "reponse4", attendu: "attendu", utilisateur: ""}
-// ]
+/* The Question class is a blueprint for creating question objects. */
+class Question {
+  constructor(questionText, choices, correctAnswer, userAnswer) {
+    this.questionText = questionText;
+    this.choices = choices;
+    this.correctAnswer = correctAnswer;
+    this.userAnswer = userAnswer;
+  }
+}
 
-// {
-//   questions: [
-//     "Le premier club de foot à avoir gagné la coupe de france est:",
-//     "Dans le film Casablanca,quelle musique l'orchestre interprète-t-il pour couvrir les chants des nazis?",
-//     "Les guêpes de mer sont?",
-//     "Laquelle de ces propositions, n’est pas une appellation du « Pierre Feuille Ciseaux » dans le monde ?",
-//     "Parmi ces 3 voitures, laquelle a vraiment existé ?",
-//   ],
-//   responses: [
-//     [
-//       "Le FC Lyon",
-//       "L'olympique de Marseille",
-//       "Le sporting de Bondy",
-//       "L'olympique de Pantin",
-//     ],
-//     [
-//       "God save the queen",
-//       "You'll never walk alone",
-//       "La marseillaise",
-//       "Lofteurs up and down",
-//     ],
-//     [
-//       "Des poissons venimeux",
-//       "Des méduses venimeuses",
-//       "Le surnom des morpions du commandant cousteau",
-//       "Des guêpes mais jetées à la mer, simplement",
-//     ],
-//     [
-//       "Klet-Klat-Klout en Finlande",
-//       "Schnick-Schnack-Schnuck en Allemagne ?",
-//       "Kawi-Bawi-Bo en Corée",
-//       "Chiam-Cham-Choum au Liban",
-//     ],
-//     ['La Skoda "Tapina"', 'La Nissan "gigolo"', 'La Mazda "la puta"'],
-//   ],
-//   responsesOk: [
-//     "L'olympique de Pantin",
-//     "La marseillaise",
-//     "Des méduses venimeuses",
-//     "Klet-Klat-Klout en Finlande",
-//     'La Mazda "la puta"',
-//   ],
-//   responsePlayer: [],
-// };
+let question1 = new Question(
+  "Le premier club de foot à avoir gagné la coupe de france est:",
+  [
+    "Le FC Lyon",
+    "L'olympique de Marseille",
+    "Le sporting de Bondy",
+    "L'olympique de Pantin",
+  ],
+  "L'olympique de Pantin"
+);
 
-// /* Creating a new element and assigning it to the variable divQCM. */
-// let divQCM = document.createElement("div");
+let question2 = new Question(
+  "Dans le film Casablanca,quelle musique l'orchestre interprète-t-il pour couvrir les chants des nazis?",
+  [
+    "God save the queen",
+    "You'll never walk alone",
+    "La marseillaise",
+    "Lofteurs up and down",
+  ],
+  "La marseillaise"
+);
 
-// /**
-//  * When the user clicks the button, the current question is removed from the page, and the next
-//  * question is added to the page.
-//  * @param e - the event object
-//  */
-// function nextQuestion(e) {
-//   e.preventDefault();
-//   /* Getting the radio buttons that have the same name as the current question, and then it is looping
-// through them to find the one that is checked. Then it is getting the id of the checked radio button
-// and pushing it to the array. */
-//   let radios = document.getElementsByName(next - 1);
-//   for (const element of radios) {
-//     if (element.checked) {
-//       divQCM.innerHTML = "";
-//       form.innerHTML = "";
-//       let value = element.id;
-//       QCM.responsePlayer.push(value);
-//       createQCM(next);
-//     }
-//   }
-// }
+let question3 = new Question(
+  "Les guêpes de mer sont?",
+  [
+    "Des poissons venimeux",
+    "Des méduses venimeuses",
+    "Le surnom des morpions du commandant cousteau",
+    "Des guêpes mais jetées à la mer, simplement",
+  ],
+  "Des méduses venimeuses"
+);
 
-// let button = document.createElement("input");
-// button.setAttribute("type", "submit");
-// button.setAttribute("value", "Valider");
-// let form = document.createElement("form");
-// divQCM.appendChild(form);
+let question4 = new Question(
+  "Laquelle de ces propositions, n’est pas une appellation du « Pierre Feuille Ciseaux » dans le monde ?",
+  [
+    "Klet-Klat-Klout en Finlande",
+    "Schnick-Schnack-Schnuck en Allemagne ?",
+    "Kawi-Bawi-Bo en Corée",
+    "Chiam-Cham-Choum au Liban",
+  ],
+  "Klet-Klat-Klout en Finlande"
+);
+let question5 = new Question(
+  "Parmi ces 3 voitures, laquelle a vraiment existé ?",
+  ['La Skoda "Tapina"', 'La Nissan "gigolo"', 'La Mazda "la puta"'],
+  'La Mazda "la puta"'
+);
+let QCM = [question1, question2, question3, question4, question5];
 
-// function finalQCM() {
-// console.log(QCM.responsePlayer)
-//   }
+/**
+ * It clears the section element, then adds the title element back to it, then calls the createQuestion
+ * function with the argument 0.
+ */
+function reStartQCM() {
+  section.innerHTML = "";
+  section.appendChild(title);
+  createQuestion(0);
+}
 
+/**
+ * It creates a div for each question, then creates a paragraph for each choice, and then adds a
+ * checkmark if the choice is correct, a line-through if the choice is wrong, and nothing if the choice
+ * is correct but not selected.
+ */
+function finalQCM() {
+  for (let i of QCM) {
+    let divQCM = document.createElement("div");
+    let para = document.createElement("p");
+    para.id = "question";
+    para.textContent = `${i.questionText}`;
+    divQCM.appendChild(para);
+    for (const choice of i.choices) {
+      let paraChoice = document.createElement("p");
+      paraChoice.id = "choices";
+      paraChoice.innerHTML = choice;
+      if (
+        (choice == i.userAnswer && i.correctAnswer == choice) ||
+        choice == i.correctAnswer
+      ) {
+        paraChoice.innerHTML += "<span>&#10003;</span>";
+        paraChoice.style.color = "rgb(30, 180, 60)";
+      } else if (choice == i.userAnswer && i.correctAnswer != i.userAnswer) {
+        paraChoice.style.color = "red";
+        paraChoice.style.textDecoration = "line-through";
+      }
+      divQCM.appendChild(paraChoice);
+    }
+    section.appendChild(divQCM);
+  }
+  let reStart = document.createElement("button");
+  reStart.innerText = "Réessayer";
+  section.appendChild(reStart);
+  reStart.addEventListener("click", reStartQCM);
+}
 
-// function createQuestion(i) {
-//   let para = document.createElement("p");
-//   para.textContent = `${QCM.questions[i]}`;
-//   divQCM.appendChild(para);
-//   body.appendChild(divQCM);
-//   console.log(QCM.responses[i]);
-//   for (const element of QCM.responses[i]) {
-//     let radio = document.createElement("input");
-//     radio.setAttribute("type", "radio");
-//     radio.setAttribute("name", i);
-//     radio.setAttribute("id", element);
-//     let label = document.createElement("label");
-//     label.setAttribute("for", element);
-//     label.textContent = element;
-//     form.appendChild(radio);
-//     form.appendChild(label);
-//     divQCM.appendChild(form);
-//   }
-// }
+/**
+ * It removes the current question, then checks if there are more questions to be asked. If there are,
+ * it creates the next question. If there aren't, it creates the final score.
+ * @param e - the event object
+ */
+function removeQuestion(e) {
+  next++;
+  e.preventDefault();
+  if (next < QCM.length) {
+    let radios = document.getElementsByName(next - 1);
+    for (const element of radios) {
+      if (element.checked) {
+        document.getElementById("divQCM").remove();
+        let value = element.id;
+        QCM[next - 1].userAnswer = value;
+        createQuestion(next);
+      }
+    }
+  } else {
+    next = 0;
+    document.getElementById("divQCM").remove();
+    finalQCM();
+  }
+}
 
-// let next = 0;
+/**
+ * It creates a form with a question and a set of choices, and adds a submit event listener to the
+ * form.
+ * @param i - the index of the question in the array
+ */
+function createQuestion(i) {
+  let divQCM = document.createElement("div");
+  divQCM.id = "divQCM";
+  let form = document.createElement("form");
+  let para = document.createElement("p");
+  para.textContent = `${QCM[i].questionText}`;
+  divQCM.appendChild(para);
+  for (const choice of QCM[i].choices) {
+    let divForm = document.createElement("div");
+    divForm.className = "divForm";
+    let radio = document.createElement("input");
+    radio.setAttribute("type", "radio");
+    radio.setAttribute("name", i);
+    radio.setAttribute("id", choice);
+    let label = document.createElement("label");
+    label.setAttribute("for", choice);
+    label.textContent = choice;
+    divForm.appendChild(radio);
+    divForm.appendChild(label);
+    form.appendChild(divForm);
+  }
+  let button = document.createElement("input");
+  button.setAttribute("type", "submit");
+  button.setAttribute("value", "Valider");
+  form.appendChild(button);
+  divQCM.appendChild(form);
+  section.appendChild(divQCM);
+  form.addEventListener("submit", removeQuestion);
+}
 
-// function createQCM(i) {
-//   if (next < QCM.questions.length) {
-//     createQuestion(i);
-//   } else {
-//     console.log("end");
-//     finalQCM();
-//   }
-//   form.appendChild(button);
-//   next++;
-// }
-
-// createQCM(0);
-
-// form.addEventListener("submit", nextQuestion);
+/* Calling the function `createQuestion` and passing the argument `0` to it. */
+createQuestion(0);
